402669050ddeedbefe9a7e24e4e5308c206cc470
Sage Rimal <srimal@campusexplorer.com>
2015-04-01 14:34:45 -0700

    Adding test

--- homework1.rb
+++ homework1.rb
@@ -2,7 +2,8 @@
 #Home work Animal Sound
 puts "Enter an animal that makes a sound:"
 animal = gets.chomp
-puts "What sound does a chicken make"
+animal = animal.downcase
+puts "What sound does a #{animal} make?"
 sound =  gets.chomp
-
+sound = sound.downcase
 puts "The #{animal} goes #{sound} #{sound} #{sound}"
