672bf018de4a0c6ed85a960e25dad7c3fce9615b
Sage Rimal <srimal@campusexplorer.com>
2015-04-01 14:29:50 -0700

    Homework 1

---
+++ homework1.rb
@@ -0,0 +1,8 @@
+
+#Home work Animal Sound
+puts "Enter an animal that makes a sound:"
+animal = gets.chomp
+puts "What sound does a chicken make"
+sound =  gets.chomp
+
+puts "The #{animal} goes #{sound} #{sound} #{sound}"
