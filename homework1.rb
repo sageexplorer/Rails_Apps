d17140f4cfe0502e46c5907d35c74e8221896266
sageexplorer <srimal@campusexplorer.com>
2015-08-10 10:54:47 -0700

    Delete homework1.rb

--- homework1.rb
+++
@@ -1,9 +0,0 @@
-
-#Home work Animal Sound
-puts "Enter an animal that makes a sound:"
-animal = gets.chomp
-animal = animal.downcase
-puts "What sound does a #{animal} make?"
-sound =  gets.chomp
-sound = sound.downcase
-puts "The #{animal} goes #{sound} #{sound} #{sound}"
